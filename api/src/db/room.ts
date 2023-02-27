1c7a375d23abc60c21f4228f59fde1275c5a0431
Calda Joselito Jr Lopez <joselito.calda@DSB.com>
2023-02-27 17:59:54 +0800

    initial commit

---
+++ api/src/db/room.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    currency: { type: String, required: true },
+    address: {},
+  },
+  { timestamps: true }
+);

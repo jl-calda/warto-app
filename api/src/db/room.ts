e96249347d7f9f6fee303ef6df07112f06250a1c
JLCalda <jlcaldadev23@gmail.com>
2023-02-27 21:53:34 +0800

    adding room schema

--- api/src/db/room.ts
+++ api/src/db/room.ts
@@ -1,12 +1,35 @@
 import mongoose from "mongoose";
 
-const AddressSchema = new mongoose.Schema(
+const RoomSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     currency: { type: String, required: true },
-    address: {},
+    availableDate: { type: String, required: true },
+    address: {
+      "block-number": { type: String, required: true },
+      "postal-code": { type: String, required: true },
+      "unit-number": { type: String, required: true },
+    },
+    notes: { type: String },
   },
   { timestamps: true }
 );
+
+export const RoomModel = mongoose.model("Room", Schema);
+
+export const getRooms = () => RoomModel.find();
+
+export const getRoomById = (id: string) => RoomModel.findById(id);
+
+export const createRoom = (values: Record<string, any>) =>
+  new RoomModel(values).save().then((room) => room.toObject());
+
+export const deleteRoomById = (id: string) =>
+  RoomModel.findByIdAndDelete({ _id: id });
